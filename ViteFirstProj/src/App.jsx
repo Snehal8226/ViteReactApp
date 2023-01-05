387abedfb991910dfd66c69e1acda351979c8d49
Snehal <117708032+Snehal8226@users.noreply.github.com>
2023-01-05 15:48:39 +0530

    First Stage

---
+++ ViteFirstProj/src/App.jsx
@@ -0,0 +1,17 @@
+import Welcome from "./components/Welcome"
+
+ export default function App() {
+    return (
+      <>
+        
+        <div>
+            Hello World
+             
+        </div>
+        <Welcome/>
+   
+
+      </>
+    );
+  }
+  
