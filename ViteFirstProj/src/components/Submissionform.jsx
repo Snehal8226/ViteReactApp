56730001f4400ede0a65e8243cccfae97a74491f
Snehal <117708032+Snehal8226@users.noreply.github.com>
2023-01-11 18:26:20 +0530

    small changes

--- ViteFirstProj/src/components/Submissionform.jsx
+++ ViteFirstProj/src/components/Submissionform.jsx
@@ -15,12 +15,72 @@ import { useState } from "react";
 
 const Submissionform=()=> {
 
-  const [age, setAge] = useState("");
+  const [firstname, setFirstName] = useState("")
+                        const onfirstnamechange =(event)=>{
+                        setFirstName(event.target.value);
+     };
 
-  const handleChange = (event) => {
-    setAge(event.target.value);
+  const [middlename, setMiddleName] = useState("")
+                         const onmiddlenamechange = (event)=>{
+                        setMiddleName(event.target.value);
+      };
+  const [lastname, setLastName] = useState("")
+                        const onlastnamechange = (event)=>{
+                          setLastName(event.target.value);
+      };
+  const [gender, setGender] = useState("")
+                        const ongenderchange = (event)=>{
+                          setGender(event.target.value);
+                        };
+
+  const [espresso,setEspresso] = useState("")
+                        const onespressochange = (event)=>{
+                          setEspresso(event.target.value);
+                        };
+                        const [cappuccino,setCappuccino] = useState("")
+                        const oncappuccinochange = (event)=>{
+                          setCappuccino(event.target.value);
+                        };
+                        const [flatwhite,setFlatWhite] = useState("")
+                        const onflatwhitechange = (event)=>{
+                          setFlatWhite(event.target.value);
+                        };
+                        const [longblack,setLongBlack] = useState("")
+                        const onlongblackchange = (event)=>{
+                          setLongBlack(event.target.value);
+                        };
+             
+  const [age, setAge] = useState("");
+                        const handleChange = (event) => {
+                         setAge(event.target.value);
   };
-    
+
+  const [developerin,setDeveloperin]=useState("")
+                  const ondeveloperinchange =(event)=>{
+                         setDeveloperin(event.target.value)
+  };
+  
+
+     const Form=()=>{
+      const FormInfo={
+          Fname:firstname,
+          Mname:middlename,
+          Lname:lastname,
+          Female:female,
+          Male:male,
+          Other:other,
+          Espresso:espresso,
+          Cappuccino:cappuccino,
+          Flatwhite:flatwhite,
+          Longblack:longblack,
+          UI:UI,
+          API:API,
+          DataBase:DataBase,
+          DevOps:DevOps
+      }
+console.log(FormInfo);
+
+     }
     return(
 
 
@@ -28,23 +88,22 @@ const Submissionform=()=> {
       <CssBaseline />
       <Container maxWidth="md">
 
-        <Box md={{ height: '100vh', padding:20 }} >
+        <Box sm={{ height: '100vh', padding:20 }} >
 
-        
-
-            <Typography>SUBMISSION FORM</Typography>
+        <Box sx={{bgcolor:"orange",display:'flex',flexDirection:'row',justifyContent:'center'}}>Submission Form</Box><br />
+            {/*<Typography>SUBMISSION FORM</Typography>*/}
             <Divider />
           
             
             <Box sx={{ display:"flex", padding:2, justifyContent:"space-between" }}>
-            <TextField id="outlined-basic" label="FirstName" variant="outlined" />
-            <TextField id="outlined-basic" label="MiddleName" variant="outlined" />
-            <TextField id="outlined-basic" label="LastName" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="FirstName" value={firstname} onChange={onfirstnamechange} variant="outlined" />
+            <TextField id="outlined-basic" label="MiddleName" value={middlename} onChange={onmiddlenamechange} variant="outlined" />
+            <TextField id="outlined-basic" label="LastName" value={lastname} onChange={onlastnamechange} variant="outlined" /><br/><br/>
             </Box>
-            
-    <FormControl>
-    <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                <RadioGroup aria-labelledby="demo-row-radio-buttons-group-label"
+       <Box sx={{display:"flex",justifyContent:"space-between" }}>     
+    <FormControl >
+    <FormLabel id="demo-row-radio-buttons-group-label" value={gender}>Gender</FormLabel>
+                <RadioGroup aria-labelledby="demo-row-radio-buttons-group-label" onChange={ongenderchange}
                   name="row-radio-buttons-group">
                   <FormControlLabel value="female" control={<Radio />} label="Female" />
                  <FormControlLabel value="male" control={<Radio />} label="Male" />
@@ -52,16 +111,21 @@ const Submissionform=()=> {
     </RadioGroup>
     </FormControl>
 
-    <FormGroup >
-    <FormLabel id="demo-row-radio-buttons-group-label">Coffee I Like</FormLabel>
-      <FormControlLabel control={<Checkbox/>} label="Espresso" />
-      <FormControlLabel control={<Checkbox/>} label="Cappuccino" />
-      <FormControlLabel control={<Checkbox/>} label="Flat White" />
-      <FormControlLabel control={<Checkbox/>} label="Long Black" />
+    <Box sx={{display:'flex'}}>
+      <FormGroup >
+    <FormLabel>Coffee I Like</FormLabel>
+    
+      <FormControlLabel value={espresso} control={<Checkbox/>} onChange={onespressochange} label="Espresso" />
+      <FormControlLabel value={cappuccino} control={<Checkbox/>} onChange={oncappuccinochange} label="Cappuccino" />
+      <FormControlLabel value={flatwhite} control={<Checkbox/>} onChange={onflatwhitechange} label="Flat White" />
+      <FormControlLabel value={longblack} control={<Checkbox/>} onChange={onlongblackchange} label="Long Black" />
       </FormGroup>
+      </Box>
+      </Box>
       
-
-      <FormControl  >
+      
+       <Box sx={{display:'flex',padding:2,justifyContent:"space-between" }}>
+      <FormControl  sx={{width:350}}>
         <InputLabel id="demo-simple-select-label">Age</InputLabel>
         <Select
           labelId="demo-simple-select-label"
@@ -78,29 +142,35 @@ const Submissionform=()=> {
           <MenuItem value={60}>60</MenuItem>
           
           </Select>
-       </FormControl><br/>
+       </FormControl>
     
 
-    <FormControl  >
-        <InputLabel id="demo-simple-select-label">Developer In</InputLabel>
+    <FormControl  sx={{width:350}}>
+        <InputLabel id="demo-simple-select-label" value={developerin}>Developer In</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
           label="Developer In"
+          onChange={ondeveloperinchange}
         >
-          <MenuItem >UI</MenuItem>
-          <MenuItem >API</MenuItem>
-          <MenuItem >DataBase</MenuItem>
-          <MenuItem >DevOps</MenuItem>
+          <MenuItem value="UI">UI</MenuItem>
+          <MenuItem value="API">API</MenuItem>
+          <MenuItem value="DataBase">DataBase</MenuItem>
+          <MenuItem value="DevOps">DevOps</MenuItem>
           </Select>
-       </FormControl><br/><br/>
+       </FormControl>
+       </Box><br/><br/>
 
+      <Box sx={{display:'flex:end'}}>
     <Button variant="contained">Submit</Button>
+    </Box>
 
-
+                   
         </Box>
       </Container>
     </React.Fragment>
+
+    
         
 
     )
